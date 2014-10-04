e68afe6f26411bed0401b3700b8a86115f761690
Joey Sabani <joey.sabani@gmail.com>
2014-10-03 23:59:16 -0500

    fixing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,14 @@ class ApplicationController < ActionController::Base
   end
 
   def format_date(date)
-    #"#{DateTime.parse(date).strftime('%D')}" 
+    "#{DateTime.parse(date).strftime('%D')}" 
   end
 
   def format_time(time)
     if time.nil?
       "still clocked in"
     else
-      time.strftime("%l:%M %P")
+     # time.strftime("%l:%M %P")
     end
   end
 
