2bde43d9af11c52959c2ccfaf812f68330852e25
Joey Sabani <joey.sabani@gmail.com>
2014-10-04 00:13:58 -0500

    fixing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     if time.nil?
       "still clocked in"
     else
-     time.strftime("%l:%M %P")
+      "#{time.strftime("%l:%M %P")}"
     end
   end
 
