462a362aae1e2fd236daa04545a45e90ed9e79e9
Joey Sabani <joey.sabani@gmail.com>
2014-10-07 11:04:42 -0500

    time entry errors r3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,16 @@ class ApplicationController < ActionController::Base
   end
 
   def format_date(date)
-    DateTime.parse(date).strftime("%D")
-    #date.strftime("%D")
+    #DateTime.parse(date).strftime("%D")
+    date.strftime("%D")
   end
 
   def format_time(time)
     if time.nil?
       "still clocked in"
     else
-      time.strftime("%l:%M %P")
-      #DateTime.parse(time).strftime("%l:%M %P")
+      #time.strftime("%l:%M %P")
+      DateTime.parse(time).strftime("%l:%M %P")
     end
   end
 
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
     hours = 0
     times.each do |entry|
       unless entry.clock_out.nil?
-        a = Time.diff(DateTime.parse(entry.clock_in), DateTime.parse(entry.clock_out))
+        a = Time.diff(entry.clock_in, entry.clock_out)
         hours += a[:hour].to_i + a[:minute]/60.0 
       end
     end
